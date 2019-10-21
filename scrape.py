229c8e1e19660ced00cbc37c947099a42e155498
Ulunma Egwim <ulunmaegwim@gmail.com>
2019-10-20 20:17:47 -0400

    Includes timestamp and inputs links into a txt file

--- scrape.py
+++ scrape.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from collections import deque
+import datetime
 
-visited = set(["http://scratchpads.eu/explore/sites-list"])
-dq = deque([["http://scratchpads.eu/explore/sites-list", "", 0]])
-max_depth = 3
+visited = set(["http://mentalfloss.com/article/53792/17-ancient-abandoned-websites-still-work"])
+dq = deque([["http://mentalfloss.com/article/53792/17-ancient-abandoned-websites-still-work", "", 0]])
+max_depth = 2
+i = 0
+urlfile = open("index.txt", "w+")
 
 while dq:
     base, path, depth = dq.popleft()
@@ -19,11 +22,19 @@ while dq:
 
                 if href not in visited:
                     visited.add(href)
-                    print("  " * depth + f"at depth {depth}: {href}")
+                    if href.startswith("http"):
+                        ts = datetime.datetime.now()
+                        ts.strftime("%m/%d/%Y")
+                        line = str(i) + ".html " + str(ts) + " " + href + "\n"
+                        urlfile.write(line)
+                        i += 1 
 
                     if href.startswith("http"):
                         dq.append([href, "", depth + 1])
                     else:
                         dq.append([base, href, depth + 1])
+                        
         except:
             pass
+
+urlfile.close()
